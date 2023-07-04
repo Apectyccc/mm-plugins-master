ccef44a1ba7a4abd24c572b1eae17755f1af44a2
Ciri <iphonandres77@gmail.com>
2023-07-04 18:57:22 -0500

    first commit

---
+++ userid_lister/userid_lister.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+import discord
+
+class addhost(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+@commands.command()
+@commands.has_role("Helper") 
+async def addhost(self, ctx, user: discord.User):
+        if user.id in self.client.host_logs:
+            old = self.client.host_logs[user.id]
+            self.client.host_logs[user.id] = old + 1
+            await ctx.send(f'Successfully added **1** host to {user.mention}.')
+        else:
+            self.client.host_logs[user.id] = 1
+    
+
+async def setup(bot):
+    await bot.add_cog(addhost(bot))
